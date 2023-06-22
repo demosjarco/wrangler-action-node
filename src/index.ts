d9e322994018d95a2a57804f9fb384deecf1cc27
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-06-22 13:14:47 -0700

    Update index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+class Wrangler {
+	constructor() {}
+
+	private execute_commands() {}
+
+	private secret_not_found() {}
+}
+
+new Wrangler();
