e60784cfd84275cb6146d6f66d4167bd88de7a6a
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-06-22 13:52:40 -0700

    Try setting up wd and npm install

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+import path from 'node:path';
+import semver from 'semver';
+import { exec } from 'node:child_process';
+
 class Wrangler {
-	constructor() {}
+	private workingDirectory?: string;
+
+	constructor() {
+		this.setupWorkingDirectory(core.getInput('workingDirectory', { trimWhitespace: true }));
+		// this.installWrangler(core.getInput('wranglerVersion', { trimWhitespace: true }));
+	}
 
 	private execute_commands() {}
 
 	private secret_not_found() {}
+
+	private setupWorkingDirectory(workingDirectory?: string) {
+		console.log('aaa', workingDirectory);
+	}
+
+	private installWrangler(version?: string) {
+		let versionToUse = '';
+
+		if (version) {
+			if (semver.valid(version)) {
+				versionToUse = `@${version}`;
+			} else {
+				console.error(`Invalid version: ${version}`, 'using currently installed or latest version');
+			}
+		}
+
+		exec(`npm install --save-dev wrangler${versionToUse}`);
+	}
 }
 
 new Wrangler();
