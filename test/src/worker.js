b8d27f092b1b883e6871e5ed647473f51c3b4f38
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-07-08 10:16:54 -0700

    Manually write out `Request` properties

--- test/src/worker.js
+++ test/src/worker.js
@@ -1,6 +1,22 @@
 export default {
 	async fetch(request, env, ctx) {
-		let temp = { headers: {} };
+		// console.log(JSON.stringify(request.cf));
+		let temp = {
+			// body: await response.clone().text(),
+			cache: request.cache,
+			cf: request.cf,
+			credentials: request.credentials,
+			destination: request.destination,
+			headers: {},
+			integrity: request.integrity,
+			method: request.method,
+			mode: request.mode,
+			redirect: request.redirect,
+			referrer: request.referrer,
+			referrerPolicy: request.referrerPolicy,
+			signal: request.signal,
+			url: request.url,
+		};
 		for (const [key, value] of request.headers.entries()) {
 			temp.headers[key] = value;
 		}
